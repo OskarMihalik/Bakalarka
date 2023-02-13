d81efc67c3c3952bd12ad1648cf74ee4573b06da
OskarMihalik <45460241+OskarMihalik@users.noreply.github.com>
2023-02-13 18:54:55 +0100

    feat: reset alarms

---
+++ Assets/ResetAlarmsButtonController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetAlarmsButtonController : MonoBehaviour
+{
+    public M2MqttPayloads m2MqttPayloads;
+
+    public void ResetAlarms()
+    {
+        m2MqttPayloads.ToggleOnePartInTopic(true, "reset_alarm", Topics.ResetAlarms);
+    } 
+}
