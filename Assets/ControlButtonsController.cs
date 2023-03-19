41ef75f5a2e7d308fba0d8587cff31ff3aab1139
OskarMihalik <45460241+OskarMihalik@users.noreply.github.com>
2023-03-19 14:43:08 +0100

    feat: show all text from buttons

--- Assets/ControlButtonsController.cs
+++ Assets/ControlButtonsController.cs
@@ -8,17 +8,26 @@ public class ControlButtonsController : MonoBehaviour
     public List<Button> controlButtonsTabs = new List<Button>();
 
     private Button activeButton;
+
+    private float rectStart;
+
+    private RectTransform rectTransform;
+
+    public float rectTransformLeft;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        foreach (var controlBurron in controlButtonsTabs)
+        foreach (var controlButton in controlButtonsTabs)
         {
-            var button = controlBurron.GetComponent<Button>();
-            button.onClick.AddListener(()=>{Activate(button);});
+            var button = controlButton.GetComponent<Button>();
+            button.onClick.AddListener(() => { Activate(button); });
         }
 
         activeButton = controlButtonsTabs[0];
         activeButton.interactable = false;
+        rectTransform = transform.GetComponent<RectTransform>();
+        rectStart = rectTransform.offsetMin.x;
     }
 
     private void Activate(Button button)
@@ -27,5 +36,34 @@ public class ControlButtonsController : MonoBehaviour
         if (activeButton != null)
             activeButton.interactable = true;
         activeButton = button;
+    }
+
+    public void TweenLeft()
+    {
+        var start = rectStart;
+        if (LeanTween.isTweening(gameObject))
+        {
+            LeanTween.cancel(gameObject);
+            start = rectTransform.offsetMin.x;
+        }
+        LeanTween.value(gameObject, start, rectTransformLeft, .5f).setOnUpdate((value) =>
+        {
+            rectTransform.offsetMin = new Vector2(value, rectTransform.offsetMin.y);
+        });
+    }
+
+    public void TweenRight()
+    {
+        var start = rectTransformLeft;
+        if (LeanTween.isTweening(gameObject))
+        {
+            LeanTween.cancel(gameObject);
+            start = rectTransform.offsetMin.x;
+        }
+
+        LeanTween.value(gameObject, start, rectStart, .5f).setOnUpdate((value) =>
+        {
+            rectTransform.offsetMin = new Vector2(value, rectTransform.offsetMin.y);
+        });
     }
 }
