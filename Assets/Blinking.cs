28b78e154e81126a94f7784216672a5995d62f09
OskarMihalik <45460241+OskarMihalik@users.noreply.github.com>
2023-05-12 12:30:43 +0200

    feat: blinking alarm

---
+++ Assets/Blinking.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blinking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var image = GetComponent<Image>();
+        LeanTween.value(gameObject, 1f, 0.3f, 0.5f).setLoopPingPong().setOnUpdate((value) =>
+        {
+            Debug.Log(value);
+            var newColor = image.color;
+            newColor.a = value;
+            image.color = newColor;
+        });
+    }
+}
